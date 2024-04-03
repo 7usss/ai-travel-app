c9d89d9f182e94ab4711822e805419028f31c133
Hussam <97773814+7usss@users.noreply.github.com>
2024-04-03 10:45:58 +0300

    Link the app to firebase
    Link the app to firebase and add allplatform to the project such as web,android.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:ai_travel/firebase_options.dart';
 import 'package:ai_travel/my_app.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
